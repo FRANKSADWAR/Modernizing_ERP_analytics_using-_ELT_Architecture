e382dc0f3420bbde6bb229bf82df4f3d110dfad7
FRANKSADWAR <billyfranks98@gmail.com>
2026-08-09 23:21:37 +0300

    simple procedure

--- table_creation.sql
+++ table_creation.sql
@@ -56,3 +56,19 @@ CREATE TABLE IF NOT EXISTS visit_history AS (
 SELECT 
     SUM(orders.revenue) AS total_revenue
 FROM {{ ref('orders') }} AS orders
+
+CREATE PROCEDURE etl_example AS 
+BEGIN
+    -- Extract data from the sales invoices table
+    SELECT * INTO #temp_table FROM `tabSales Invoice`;
+
+    -- Transform the data
+    UPDATE #temp_table
+    SET last_updated = CURDATE(),
+        customer_name = UPPER(customer_name);
+
+    -- Load the data into a target table
+    INSERT INTO target_table
+    SELECT * FROM #temp_table;
+
+END
