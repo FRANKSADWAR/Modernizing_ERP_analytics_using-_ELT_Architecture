46c0b29c625630e3d160fcc89966b67ca4b0ad50
FRANKSADWAR <billyfranks98@gmail.com>
2026-08-10 22:47:26 +0300

    dbt start

--- table_creation.sql
+++ table_creation.sql
@@ -50,25 +50,3 @@ CREATE TABLE IF NOT EXISTS visit_history AS (
     FOREIGN_KEY(channel_id) REFERENCES channels(channel_id),
     FOREIGN_KEY(customer_id) REFERENCES customers(customer_id)
 );
-
-{{ config(materialized = 'table') }}
-
-SELECT 
-    SUM(orders.revenue) AS total_revenue
-FROM {{ ref('orders') }} AS orders
-
-CREATE PROCEDURE etl_example AS 
-BEGIN
-    -- Extract data from the sales invoices table
-    SELECT * INTO #temp_table FROM `tabSales Invoice`;
-
-    -- Transform the data
-    UPDATE #temp_table
-    SET last_updated = CURDATE(),
-        customer_name = UPPER(customer_name);
-
-    -- Load the data into a target table
-    INSERT INTO target_table
-    SELECT * FROM #temp_table;
-
-END
