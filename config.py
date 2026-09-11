a8d1a0d08f9f2073452c7542d9e8318bcc81ad80
FRANKSADWAR <billyfranks98@gmail.com>
2026-09-11 11:59:09 +0300

    added config for table names

---
+++ config.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+
+
+TABLE_LIST = ['Customer','Sales Team','Supplier','Contact','Address','Employee',
+              'Sales Order','Sales Invoice','Delivery Note','Credit Limit','Purchase Order',
+              'Purchase Invoice','Purchase Receipt','Journal Entry','Journal Entry Reference','GL Entry','Account',
+              'Payment Entry','Payment Entry Reference','Dynamic Link','Stock Entry','Work Order','Stock Ledger Entry',
+              'Sales Order Item','Delivery Note Item','Sales Invoice Item','Purchase Order Item','Purchase Receipt Item',
+              'Purchase Invoice Item','Item','Warehouse','Salary Slip','Stock Reconciliation','Stock Reconciliation Item',
+              'Attendance','Item Group'
+              ]
